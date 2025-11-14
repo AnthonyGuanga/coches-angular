9af828312315f2234d31ec87d537ac7219ad4073
anthonyGuanga <ninfrad23@gmail.com>
2025-11-14 19:59:20 +0100

    add ficheros para el comienzo se creo plantillas principal,

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,22 @@
 import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component'
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    CommonModule,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('Angular-coches');
- 
+
 }
