41b290a88fe28ee403e60f23a5620c487793c502
anthonyGuanga <ninfrad23@gmail.com>
2025-11-18 21:59:17 +0100

    add primer demo del slider carousel, se implemento la libreria de swiper, se creo carpeta de componentes

---
+++ src/app/components/carousel/carousel.component.ts
@@ -0,0 +1,24 @@
+import { Component, Input, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { register } from 'swiper/element/bundle';
+
+// Registrar Swiper
+register();
+
+@Component({
+  selector: 'app-carousel',
+  imports: [],// Está vacío porque usamos la sintaxis moderna @if/@for
+  // Esto permite usar etiquetas "inventadas" como <swiper-container>
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  //schemas: [CUSTOM_ELEMENTS_SCHEMA]: Angular es muy estricto y si ve etiquetas que no conoce, da error. Esta línea le dice a Angular: 
+  //"Relájate, voy a usar etiquetas externas (Web Components), no te preocupes por validar todo".
+  templateUrl: './carousel.component.html',
+  styleUrl: './carousel.component.css',
+  host: { 'ngSkipHydration': 'true' }
+})
+
+
+export class CarouselComponent {
+  // Aquí está la magia: @Input permite recibir datos desde fuera
+  @Input() slides: any[] = [];
+}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
-
+import { CarouselComponent } from '../components/carousel/carousel.component'; // Importas tu hijo
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [CarouselComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
 export class HomeComponent {
-
+  // Datos para la portada
+  bannerImages = [
+    { img: 'https://mlin.es/wp-content/uploads/2024/08/marcas-de-coches-de-lujo-poco-conocidas-1024x626.jpg', title: '1' },
+    { img: 'https://mlin.es/wp-content/uploads/2024/08/Marcas-raras-de-coches-pagani-1024x576.webp', title: '2' },
+    { img: 'https://mlin.es/wp-content/uploads/2024/08/Koenigsegg-super-marca-de-lujo-de-coches.jpg', title: '3' },
+  ];
 }
