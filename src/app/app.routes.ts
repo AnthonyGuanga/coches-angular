28fa26161e07a8ff27bd01f89b9c473d32857fad
anthonyG <you@example.com>
2025-11-27 11:29:13 +0100

    add se creo dos nuevos componentes login y category comenzando el router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { CategoryComponent } from './components/category/category.component';
+import { LoginComponent } from './components/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '',component: HomeComponent},
+    {path: 'category', component: CategoryComponent},
+    {path: 'login', component: LoginComponent}
+];
