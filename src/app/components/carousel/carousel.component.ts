e4c202829d5fcaea4c28be483f11ef8db0385d5b
anthonyGuanga <ninfrad23@gmail.com>
2026-02-16 18:31:42 +0100

    fix del carrusel

--- src/app/components/carousel/carousel.component.ts
+++ src/app/components/carousel/carousel.component.ts
@@ -1,24 +1,18 @@
 import { Component, Input, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { register } from 'swiper/element/bundle';
 
-// Registrar Swiper
 register();
 
 @Component({
   selector: 'app-carousel',
-  imports: [],// Está vacío porque usamos la sintaxis moderna @if/@for
-  // Esto permite usar etiquetas "inventadas" como <swiper-container>
+  standalone: true, // Angular 17+ suele ser standalone por defecto, pero es bueno ser explícito
+  imports: [],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
-  //schemas: [CUSTOM_ELEMENTS_SCHEMA]: Angular es muy estricto y si ve etiquetas que no conoce, da error. Esta línea le dice a Angular: 
-  //"Relájate, voy a usar etiquetas externas (Web Components), no te preocupes por validar todo".
   templateUrl: './carousel.component.html',
   styleUrl: './carousel.component.css',
+  // ngSkipHydration ayuda si usas SSR (Server Side Rendering) para evitar conflictos con el DOM de Swiper
   host: { 'ngSkipHydration': 'true' }
 })
-
-
 export class CarouselComponent {
-  // Aquí está la magia: @Input permite recibir datos desde fuera
   @Input() slides: { img: string; title?: string }[] = [];
 }
